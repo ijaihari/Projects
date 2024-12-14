3a98cd9035d7acf863daefd5af97e810cfb581b2
Jai Hari Nataraj <sjaiharinataraj@gmail.com>
2024-12-14 21:50:38 +0530

    make cart quantity interactive

--- Amazon Clone/scripts/amazon.js
+++ Amazon Clone/scripts/amazon.js
@@ -3,7 +3,7 @@
 let productsHTML = '';
 
 products.forEach((product) => {
-  productsHTML +=   ` <div class="product-container">
+  productsHTML += ` <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
               src="${product.image}">
@@ -15,7 +15,7 @@ products.forEach((product) => {
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars*10}.png">
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
              ${product.rating.count}
             </div>
@@ -47,17 +47,42 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary js-add-to-cart">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>`
-        
+
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
-button.addEventListener('click',()=>{
-  console.log('Added to Cart');
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  button.addEventListener('click', () => {
+    const productId = button.dataset.productId;
+    let matchingItem;
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    }
+    else {
+      cart.push({
+        productId: productId,
+        quantity: 1
+      });
+    }
+let cartQuantity =0;
+
+cart.forEach((item)=>{
+cartQuantity += item.quantity
 });
+
+ document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+
+    console.log(cartQuantity);
+    console.log(cart);
+  });
 })
