6b6e2854712a61e1e9ec4c8f80dc813fa70966a7
Jai Hari Nataraj <sjaiharinataraj@gmail.com>
2024-12-14 07:02:45 +0530

    added cart event

--- Amazon Clone/scripts/amazon.js
+++ Amazon Clone/scripts/amazon.js
@@ -47,7 +47,7 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart">
             Add to Cart
           </button>
         </div>`
@@ -55,3 +55,9 @@ products.forEach((product) => {
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
+button.addEventListener('click',()=>{
+  console.log('Added to Cart');
+});
+})
