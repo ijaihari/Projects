0286b188b79055916e49a4deefe095503cfbfe8f
Jai Hari Nataraj <sjaiharinataraj@gmail.com>
2024-12-15 15:50:53 +0530

    created quanity count by select element

--- Amazon Clone/scripts/amazon.js
+++ Amazon Clone/scripts/amazon.js
@@ -26,7 +26,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-product-quantity-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -55,10 +55,12 @@ products.forEach((product) => {
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
-
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
     const productId = button.dataset.productId;
+    const productQuantity = Number(document.querySelector(`.js-product-quantity-${productId}`).value);
+
+    console.log(productQuantity);
     let matchingItem;
     cart.forEach((item) => {
       if (productId === item.productId) {
@@ -66,23 +68,22 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       }
     });
     if (matchingItem) {
-      matchingItem.quantity += 1;
+      matchingItem.quantity += productQuantity;
     }
     else {
       cart.push({
         productId: productId,
-        quantity: 1
+        quantity: productQuantity
       });
     }
-let cartQuantity =0;
 
-cart.forEach((item)=>{
-cartQuantity += item.quantity
-});
+    /* Navbar Cart Quantity update */
+    let cartQuantity = 0;
 
- document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity
+    });
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
 
-    console.log(cartQuantity);
-    console.log(cart);
   });
 })
